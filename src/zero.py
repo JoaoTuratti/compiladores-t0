38204567d6d5db76a2b2a71759dce900c82dc051
João <joaoguilhermeturattire@gmail.com>
2024-05-22 19:15:48 -0300

    final

--- src/zero.py
+++ src/zero.py
@@ -5,8 +5,24 @@
 
 def procura_maior(lista):
     """Procura maior item na lista usando procura linear."""
-    maior = lista[0]
-    for item in lista[1:]:
-        if item > maior:
-            maior = item
-    return maior
+    return max(lista)
+
+def procura_menor(lista):
+    """Procura o menor item na lista."""
+    return min(lista)
+
+def procura_impares(lista):
+    """Procura numeros impares."""
+    impares = []
+    for item in lista:
+        if (item%2) != 0:
+            impares.append(item)
+    return impares
+
+def procura_pares(lista):
+    """Procura numeros pares."""
+    pares = []
+    for item in lista:
+        if (item%2) == 0:
+            pares.append(item)
+    return pares
